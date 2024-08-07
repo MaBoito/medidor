184b40b501007dafed215aeb0df48304325db013
MaBoito <matheus_boito@hotmail.com>
2024-08-07 14:35:39 -0300

    add const

--- scripts.js
+++ scripts.js
@@ -2,10 +2,11 @@ verificarVelocidade (84);
 
 function verificarVelocidade(velocidade) {
     const velocidadeMaxima = 70;
+    const kmPorPonto = 5;
 if (velocidade <= velocidadeMaxima )
     console.log("ok");
 else {
-    const pontos = Math.floor (((velocidade - velocidadeMaxima)/ 5));
+    const pontos = Math.floor (((velocidade - velocidadeMaxima)/ kmPorPonto));
     if ( pontos >= 12)
         console.log("Carteira suspensa");
     else
